987eaa084a49fb78e78caabb55c9c77a83d9ac8a
Ryuofficial <104766474+Ryuofficial@users.noreply.github.com>
2025-02-12 23:37:21 +0800

    feat: enhance PresetPage component with improved type safety and download options

--- src/components/PresetPage.tsx
+++ src/components/PresetPage.tsx
@@ -1,7 +1,9 @@
 import { usePresets } from "@/hooks/usePresets";
+import { dir } from "node:console";
+import React from "react";
 import { useParams } from "react-router-dom";
 
-const PresetPage = () => {
+const PresetPage: React.FC = () => {
   const { presets } = usePresets();
   const { prstid } = useParams(); // Get the index from URL params
 
@@ -15,23 +17,26 @@ const PresetPage = () => {
     );
   }
 
+  const { title, author, img, DownloadMusic, DownloadXML, directAM, genre } =
+    preset;
+
   return (
-    <div className="h-full overflow-auto w-screen  flex text-white justify-center items-center  ">
+    <div className="h-screen overflow-auto w-screen  flex text-white justify-center items-center  ">
       <div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:max-w-7xl pt-16 md:pt-8 h-screen w-screen px-0 md:px-8 ">
+        <div className=" h-fit grid grid-cols-1 sm:grid-cols-2 md:max-w-7xl pt-16 md:pt-8 w-screen px-0 md:px-8 ">
           <div className="w-full p-8">
             <div>
               <img
                 className="rounded-xl backdrop-blur-xl bg-white/5   border border-white/10 shadow-xl hover:shadow-2xl transition-all duration-300"
-                src={preset.img}
-                alt={preset.title}
+                src={img}
+                alt={title}
               />
             </div>
             <div className="mt-4 bg-slate-900  backdrop-blur-xl bg-white/5 p-6 rounded-2xl border border-white/10 shadow-xl hover:shadow-2xl transition-all duration-300">
               <div className="space-y-2">
                 {/* Title */}
                 <h1 className="text-[clamp(1.1rem,2vw,1.5rem)] font-bold  bg-gradient-to-r from-[#2af598]  to-[#009efd] text-transparent bg-clip-text">
-                  {preset.title}
+                  {title}
                 </h1>
                 {/* Genre */}
                 <div className="space-x-2 text-[clamp(.7rem,2vw,.9rem)] ">
@@ -42,7 +47,7 @@ const PresetPage = () => {
                     id="genre-label"
                     className="inline-flex items-center rounded-full border px-2.5 py-0.5 transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent text-[clamp(.6rem,2vw,.6rem)] font-medium bg-white/10 text-white"
                   >
-                    {preset.genre}
+                    {genre}
                   </div>
                 </div>
                 {/* Author */}
@@ -51,7 +56,7 @@ const PresetPage = () => {
                     Author:
                   </label>
                   <p className="text-[clamp(.7rem,2vw,.5rem)] text-white  font-medium ">
-                    {preset.author}
+                    {author}
                   </p>
                 </div>
               </div>
@@ -59,18 +64,32 @@ const PresetPage = () => {
           </div>
           <div className="w-full p-8 pt-0 md:pt-8">
             <div>
-              <h2 className="text-[clamp(1.2rem,2vw,1.7rem)] font-semibold text-white mb-4">
+              <h2 className="text-[clamp(1rem,2vw,1.7rem)] font-semibold text-white mb-4">
                 Download Options
               </h2>
 
               <div className="space-y-2 text-[clamp(.7rem,2vw,.9rem)] ">
-                <button className="download-preset-button">Download XML</button>
-                <button className="download-preset-button">
+                <a
+                  href={DownloadXML}
+                  target="_blank"
+                  className="download-preset-button"
+                >
+                  Download XML
+                </a>
+                <a
+                  href={DownloadMusic}
+                  target="_blank"
+                  className="download-preset-button"
+                >
                   Download Music
-                </button>
-                <button className="download-preset-button">
+                </a>
+                <a
+                  href={directAM}
+                  target="_blank"
+                  className="download-preset-button"
+                >
                   Import Directly
-                </button>
+                </a>
               </div>
             </div>
           </div>
