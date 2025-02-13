0302280cdca2fb8313e6f889b256b9b0e602aafe
Ryuofficial <104766474+Ryuofficial@users.noreply.github.com>
2025-02-13 19:13:04 +0800

    feat: add react-router-hash-link for smooth scrolling navigation

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,3 +1,4 @@
+import { HashLink } from "react-router-hash-link";
 import { FiMenu, FiX } from "react-icons/fi";
 import { useState } from "react";
 import Logo from "../img/logo.webp";
@@ -30,24 +31,32 @@ function Nav() {
           <div className=" z-10 border border-t-[0px] border-r-[0px] border-l-[0px] border-[#151722] md:border-none text-[#bebebe] nav-links-bar  duration-500 md:static absolute bg-[#0e0f16] py-10 md:py-0 md:min-h-fit min-h-min left-0 top-[-100%] md:w-auto w-full flex item-center px-10">
             <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
               <li>
-                <a className="hover:text-[#2af598]" href="#About">
+                <HashLink className="hover:text-[#2af598]" smooth to="#About">
                   About
-                </a>
+                </HashLink>
               </li>
               <li>
-                <a className="hover:text-[#2af598]" href="#features">
+                <HashLink
+                  className="hover:text-[#2af598]"
+                  smooth
+                  to="#features"
+                >
                   Features
-                </a>
+                </HashLink>
               </li>
               <li>
-                <a className="hover:text-[#2af598]" href="#faqs">
+                <HashLink className="hover:text-[#2af598]" smooth to="#faqs">
                   Faqs
-                </a>
+                </HashLink>
               </li>
               <li>
-                <a className="hover:text-[#2af598]" href="#Contact-section">
+                <HashLink
+                  className="hover:text-[#2af598]"
+                  smooth
+                  to="#Contact-section"
+                >
                   Contact
-                </a>
+                </HashLink>
               </li>
             </ul>
           </div>
